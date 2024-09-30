8cfedd04cb66a522caf525689cbb9725ba1063c5
llanskoyll <artem.maximov.2000@bk.ru>
2024-09-30 18:11:02 +0300

    инициализировал проект

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "userInterface/cli.h"
+
+int main() {
+    CLI app;
+
+    app.print();
+
+    return 0;
+}
---
+++ src/userInterface/cli.cpp
@@ -0,0 +1,7 @@
+#include "userInterface/cli.h"
+
+#include <iostream>
+
+void CLI::print() {
+    std::cout << "Debug print\n";
+}
