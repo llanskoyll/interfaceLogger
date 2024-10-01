132d2ca5e4d456423d9a8b8b318d01cee29fa2dc
llanskoyll <artem.maximov.2000@bk.ru>
2024-10-01 22:46:39 +0300

    поправил вывод на наличие только протокола и порта

--- src/network/network_model.cpp
+++ src/network/network_model.cpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <cstdio>
 #include <memory>
+#include <algorithm>
 
 namespace network {
 
@@ -16,22 +17,40 @@ bool NetworkModel::setPort(uint32_t port) {
 }
 
 std::string NetworkModel::listOpenPortsInSystem() {
-    std::string ports;
+    std::vector<std::string> ports;
+    std::vector<std::string> protocols;
+    std::string result;
 
-    std::array<char, 128> buffer;
+    std::array<char, 64> buffer;
     std::string port;
+    std::string command("ss -tuln | awk '{print $1, $5}' | grep -oE '[A-Za-z]+|:[0-9]+'");
 
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("ss -tuln", "r"), pclose);
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.data(), "r"), pclose);
     if (!pipe) {
         std::cerr << "Не удалось получить список открытых портов" << std::endl;
         exit(1);
     }
 
     while (std::fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        ports += buffer.data();
+        std::string line(buffer.data());
+
+        line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
+
+        if (line.find(":") != std::string::npos) {
+            ports.push_back(line.substr(1));
+        } else {
+            protocols.push_back(line);
+        }
     }
 
-    return ports;
+    for (size_t i = 0; i < protocols.size() && i < ports.size(); ++i) {
+        result += protocols[i];
+        result += " ";
+        result += ports[i];
+        result += "\n";
+    }
+
+    return result;
 }
 
-}
+} // namespace network
